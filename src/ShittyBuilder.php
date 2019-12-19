0d73e2c49674aea04d7f595db86334dd6e364dfe
dasauser <dasauser@gmail.com>
2019-12-19 18:35:30 +0300

    change access modifier to protected;

--- src/ShittyBuilder.php
+++ src/ShittyBuilder.php
@@ -3,43 +3,43 @@ namespace Dasauser;
 
 class ShittyBuilder
 {
-    public static $assets_dir;
-    public static $built_dir;
-    public static $built_map;
+    protected static $assets_dir;
+    protected static $built_dir;
+    protected static $built_map;
 
     public static function check(string $assets_dir, array $build_map_dir = [], string $built_dir = 'public/build') : void
     {
-        self::$assets_dir = $assets_dir;
-        self::$built_dir = $built_dir;
-        if (!file_exists(self::$built_dir)) {
-            mkdir(self::$built_dir, 0755, true);
+        static::$assets_dir = $assets_dir;
+        static::$built_dir = $built_dir;
+        if (!file_exists(static::$built_dir)) {
+            mkdir(static::$built_dir, 0755, true);
         }
-        $build_map_array = $build_map_dir === [] ? self::getFilesMap() : $build_map_dir;
-        $built_map = self::getBuildedMap();
+        $build_map_array = $build_map_dir === [] ? static::getFilesMap() : $build_map_dir;
+        $built_map = static::getBuildedMap();
         $updated = false;
         foreach ($build_map_array as $item) {
-            $file_name = self::$assets_dir . "/$item";
+            $file_name = static::$assets_dir . "/$item";
             if (file_exists($file_name)) {
                 $md_hash = md5_file($file_name);
                 if ($md_hash !== $built_map[$item]['hash']) {
                     $built_map[$item]['hash'] = $md_hash;
                     if (isset($item['built_file'])) {
-                        self::deleteOld($item['built_file']);
+                        static::deleteOld($item['built_file']);
                     }
                     $updated = true;
                 } else {
                     if (empty($built_map[$item]['built_file'])) {
                         $updated = true;
                     } else {
-                        $built_file = self::$built_dir . '/' . $built_map[$item]['built_file'];
+                        $built_file = static::$built_dir . '/' . $built_map[$item]['built_file'];
                         if (!file_exists($built_file)) {
-                            self::copy($file_name, $built_map[$item]['built_file']);
+                            static::copy($file_name, $built_map[$item]['built_file']);
                         }
                     }
                 }
                 if ($updated) {
-                    if (($built_map[$item]['built_file'] = self::copy($file_name)) !== '') {
-                        self::updateContent(self::$built_dir . '/' . $built_map[$item]['built_file']);
+                    if (($built_map[$item]['built_file'] = static::copy($file_name)) !== '') {
+                        static::updateContent(static::$built_dir . '/' . $built_map[$item]['built_file']);
                     } else {
                         $updated = false;
                     }
@@ -49,32 +49,32 @@ class ShittyBuilder
             }
         }
         if ($updated) {
-            self::write($built_map);
+            static::write($built_map);
         }
     }
 
-    public static function getFilesMap() : array
+    protected static function getFilesMap() : array
     {
-        $file_map = self::$assets_dir . '/map.php';
+        $file_map = static::$assets_dir . '/map.php';
         if (file_exists($file_map)) {
             return require_once($file_map);
         }
-        throw new \Exception('Set custom map file or create ' . self::$assets_dir . '/map.php', 400);
+        throw new \Exception('Set custom map file or create ' . static::$assets_dir . '/map.php', 400);
     }
 
-    public static function getBuildedMap() : array
+    protected static function getBuildedMap() : array
     {
-        self::$built_map = self::$built_dir . '/map.json';
-        if (file_exists(self::$built_map)) {
-            return json_decode(file_get_contents(self::$built_map), true);
+        static::$built_map = static::$built_dir . '/map.json';
+        if (file_exists(static::$built_map)) {
+            return json_decode(file_get_contents(static::$built_map), true);
         }
         return [];
     }
 
-    public static function copy(string $file_name, string $new_file = '') : string
+    protected static function copy(string $file_name, string $new_file = '') : string
     {
         $new_file = $new_file === '' ? substr(md5(uniqid()), -15) . substr($file_name, strripos($file_name, '.')) : $new_file;
-        $new_file_path = self::$built_dir . '/' . $new_file;
+        $new_file_path = static::$built_dir . '/' . $new_file;
         if (!copy($file_name, $new_file_path)) {
             return '';
         }
@@ -84,7 +84,7 @@ class ShittyBuilder
         return $new_file;
     }
 
-    public static function updateContent(string $file) : void
+    protected static function updateContent(string $file) : void
     {
         $file_content = file_get_contents($file);
         $curl = curl_init();
@@ -98,14 +98,14 @@ class ShittyBuilder
         file_put_contents($file, json_decode($result, true)['minified']);
     }
 
-    public static function write(array $data) : void
+    protected static function write(array $data) : void
     {
-        file_put_contents(self::$built_map, json_encode($data));
+        file_put_contents(static::$built_map, json_encode($data));
     }
 
-    public static function deleteOld(string $old_file_name) : void
+    protected static function deleteOld(string $old_file_name) : void
     {
-        $old_file_name = self::$built_dir . "/$old_file_name";
+        $old_file_name = static::$built_dir . "/$old_file_name";
         if (file_exists($old_file_name)) {
             unlink($old_file_name);
         }
